3df1791b99f6f070a7dc71227a0e03cdbe805291
Your Name <youremail@example.com>
2024-02-28 19:44:25 +0500

    Assignment 15

---
+++ app.js
@@ -0,0 +1,89 @@
+// Chapter 6 
+
+// 1
+x++;
+document.write("1. Short form of x = x + 1: " + x + "<br>");
+
+// 2
+x--;
+document.write("2. Fastest way to reduce x from 100 to 99: " + x + "<br>");
+
+// 3. 
+var x = 50;
+var y = x++;
+document.write("3. Value of y: " + y + "<br>");
+
+// 4. 
+var y = 50;
+var z = --y;
+document.write("4. Value of z: " + z + "<br>");
+
+// 5. 
+newNum = num--;
+document.write("5. Value of newNum: " + newNum + "<br>");
+
+// 6. 
+var originalValue = variable++;
+document.write("6. Original value of variable: " + originalValue + "<br>");
+
+// 7. 
+var number = 10;
+number++;
+alert(number);
+
+
+// Chapter 7 
+
+// 1. 
+var calculatedNum = 2 + (2 * 6);
+document.write("1. Value of calculatedNum: " + calculatedNum + "<br>");
+
+// 2. 
+var calculatedNum = (2 + 2) * 6;
+document.write("2. Value of calculatedNum: " + calculatedNum + "<br>");
+
+// 3. 
+var calculatedNum = (2 + 2) * (4 + 2);
+document.write("3. Value of calculatedNum: " + calculatedNum + "<br>");
+
+// 4. 
+var calculatedNum = ((2 + 2) * 4) + 2;
+document.write("4. Value of calculatedNum: " + calculatedNum + "<br>");
+
+// 5. 
+var cost = (2 + 2) * (4 + 10);
+document.write("5. Value of cost: " + cost + "<br>");
+
+// 6. 
+var units = 2 + (2 * (4 + 10));
+document.write("6. Value of units: " + units + "<br>");
+
+// 7. 
+var pressure = (4 / 2) * 4;
+document.write("7. Value of pressure: " + pressure + "<br>");
+
+
+// Chapter 8 
+
+// 1. 
+var num = "2" + "2";
+document.write("1. Value of num: " + num + "<br>");
+
+// 2. 
+var message = ("Hello," + "Dolly");
+alert(message);
+
+// 3. 
+alert("33" + 3);
+
+// 4. 
+alert("Pakistan " + "Zindabad");
+
+// 5. 
+var result = "String" + 123;
+document.write("5. Value of result: " + result + "<br>");
+
+// 6. 
+var str1 = "Hello";
+var str2 = "World";
+var combinedStr = str1 + str2;
